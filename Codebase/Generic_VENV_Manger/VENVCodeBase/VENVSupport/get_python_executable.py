c46ecb25e3d38be6a0e61321065bdcea8b60a28f
unknown <ConvexBurrito5@gmail.com>
2025-03-01 15:36:18 -0500

    update

--- Codebase/Generic_VENV_Manger/VENVCodeBase/VENVSupport/get_python_executable.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_python_executable(venv_destination_folder):
-    """Get the correct python executable inside the virtual environment."""
-    if os.name == "nt":  # Windows
-        return os.path.join(venv_destination_folder, "Scripts", "python.exe")
-    else:  # macOS/Linux
-        return os.path.join(venv_destination_folder, "bin", "python")
