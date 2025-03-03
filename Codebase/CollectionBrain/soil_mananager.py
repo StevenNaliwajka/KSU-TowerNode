9bcc9c7cee5efc723e5ca24a88f3150f8c3aefea
unknown <ConvexBurrito5@gmail.com>
2025-03-03 14:40:18 -0500

    update

--- Codebase/CollectionBrain/soil_mananager.py
+++ Codebase/CollectionBrain/soil_mananager.py
@@ -1,7 +1,12 @@
+from datetime import time
+
 from Codebase.DataCollection.Connection.soil_connection import SoilConnection
 
 
 def soil_mananager():
     soil_connection = SoilConnection()
 
-    for time
+    while True:
+        soil_connection.log_soil_data()
+        soil_connection.display()
+        time.sleep(1)
