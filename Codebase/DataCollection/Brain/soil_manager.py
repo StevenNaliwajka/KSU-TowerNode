296d7a02e202d737764e5152dbef418fbdaf7a57
unknown <ConvexBurrito5@gmail.com>
2025-03-05 06:25:20 -0500

    update

--- Codebase/DataCollection/Brain/soil_manager.py
+++ Codebase/DataCollection/Brain/soil_manager.py
@@ -1,9 +1,12 @@
 import sys
 from pathlib import Path
-import time
 
-# Ensure the project root is in sys.path
-sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+# Dynamically find the project root, required to allow for correct imports.
+folder_ct = 3  ### NOT always 2. Change to the qty of folders up before root.
+PROJECT_ROOT = Path(__file__).resolve().parents[folder_ct]
+sys.path.append(str(PROJECT_ROOT))
+
+import time
 
 from Codebase.DataCollection.Connection.soil_connection import SoilConnection
 
